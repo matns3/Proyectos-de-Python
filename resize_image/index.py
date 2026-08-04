5a313b569d31f39224827ec15356e4d21ea2bb0b
Matías Suárez <mnsmattun3@gmail.com>
2026-08-04 12:10:52 -0300

    Update index.py

--- resize_image/index.py
+++ resize_image/index.py
@@ -1,1 +1,25 @@
+#Create toolbox to edit images
+from PIL import Image
 
+#Load image
+img = Image.open("wallpaper.jpg")
+"""
+Image.open = in the "Image" toolbox, use (.) the "open" tool to load image
+"""
+
+#Resize image
+new = img.resize((400, 300))
+"""
+img.resize = resize the image that is in "img" variable
+(400, 300) = tuple
+"""
+
+#Save the new image
+new.save("new_wallpaper.jpg")
+"""
+new.save = save the new image that is in "new" variable
+"""
+
+#Print original and new size
+print("Original size:", img.size)
+print("New size:", new.size)
